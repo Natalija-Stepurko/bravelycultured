6a8f570d665875fd60384c88f1b4338bbc2fd7d4
Natalija Stepurko <natalija.stepurko@gmail.com>
2024-05-20 14:32:57 +0100

    <meta property=og:image content=%PUBLIC_URL%/BravelyCultured.png />

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Banner.css'; // Import the CSS file for styles
-import bannerImage from '../../assets/banner_splashes.png';
+import bannerImage from '../../assets/banner.png';
 
 
 const Banner = ({ text }) => {
   return (
     <div className="banner">
         <img src={bannerImage} alt="Banner" />
-        <h1>Marine microbes + Industrial Waste = Green chemicals</h1>
-        <h2>On a mission to make biotech commodities replacing petroleum-derived products</h2>
+        <h1>Ocean-based Chemicals for <br />  Personal and Home Care</h1>
+        <h2>Kind to the skin, and the planet</h2>
         <a href="https://www.linkedin.com/company/bravelycultured" className="linkedin-button">Find us on LinkedIn</a>
     </div>
   );
